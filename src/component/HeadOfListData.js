5780cdba1e8c3cc2dbc97581d33eb5cebb94f282
tonylo1128 <tonylo19961128@gmail.com>
2020-04-17 00:27:54 +0800

    new per_page function

--- src/component/HeadOfListData.js
+++ src/component/HeadOfListData.js
@@ -19,23 +19,9 @@ function HeadOfListData({route,busCom,dst }) {
             </div>
 
 
-            <div className="flexDCol">
-
-                <div className="trapezoid"> 
-                    <div className="cityBusLogo"> </div>
-                    {/* <div className="kmbBusLogo"> </div> */}
-                </div>
-
-                <div className="RTG"> 
-                   {/* <div className="fontSetting"> {dst} </div> */}
-                   {/* {dst} */}
-                </div>
-
-                <div className="RTGNo2"> 
-                    <div className="RTGNo3"> {route}  </div>
-                </div>
-                    
-            </div>
+             
+            
+            
 
 
 
